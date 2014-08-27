458ec4ce3714af219cf3f5454ff55d48dbf0f3e2
Abhinav <sharma.abhinav@ideametrics.co.in>
2014-08-27 12:36:03 +0000

    Adding test Project

---
+++ Substract/Substract/Model1/MyModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SubstractLogic;
+using System.Web;
+
+namespace Substract.Model1
+{
+    public class MyModels
+    {
+        public ISubstract _substract;
+
+
+        public MyModels(ISubstract substract)
+        {
+            _substract = substract;
+        }
+    }
+}
---
+++ Substract/TestLib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rhino.Mocks;
+using Substract.Model1;
+using SubstractLogic;
+using NUnit.Framework;
+namespace TestLib
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyModelTest()
+        {
+            var subsMock = MockRepository.GenerateStub<ISubstract>();
+
+            var model = new MyModels(subsMock);
+
+            Assert.AreEqual(subsMock, model._substract);
+        }
+    }
+}
